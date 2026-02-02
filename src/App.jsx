40584b1c01484e8d8138e72b092bfa4dd6665ae2
DevIgorRafaelDeMelo <rafaelmelo765@gmail.com>
2026-02-02 13:41:04 -0300

    V1.1 02.02.2026

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import React from "react";
 import Header from "./Conponetes/Header";
 import Footer from "./Conponetes/Footer";
@@ -14,7 +13,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </div>
--- src/Pages/About.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function About() {
-  return <h1 className="text-2xl font-bold">Sobre o Projeto</h1>;
-}
