e509761e893dae444cfbfeaf312be734ec1f53c1
DevIgorRafaelDeMelo <rafaelmelo765@gamil.com>
2026-05-27 15:37:19 -0300

    V1.0 27.05.26

--- src/Conponetes/Whats.jsx
+++ src/Conponetes/Whats.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/5511999999999" // coloque seu número aqui
+      href="https://wa.me/5551998957775" //
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 flex items-center gap-2 px-6 py-3 rounded-full bg-blue-500 hover:bg-blue-600 text-white font-semibold shadow-lg shadow-blue-500/40 transition transform hover:scale-105 z-50"
